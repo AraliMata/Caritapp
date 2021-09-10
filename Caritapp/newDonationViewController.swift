cb7403edefcd0d3263402ac9343eef90da796d44
Aralí Mata <aralipdf@gmail.com>
2021-09-09 22:10:33 -0400

    BDA-1 Pantalla de creación de donación :lipstick:

---
+++ Caritapp/newDonationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  newDonationViewController.swift
+//  Caritapp
+//
+//  Created by user189928 on 9/9/
+
+import UIKit
+
+class newDonationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
