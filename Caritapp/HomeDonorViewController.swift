6c6b1cfc719edbe3c1f74623a722b396be86e540
Laurie Diego <laurievalediego@gmail.com>
2021-10-13 14:04:11 -0500

    Calendar Table

--- Caritapp/HomeDonorViewController.swift
+++ Caritapp/HomeDonorViewController.swift
@@ -12,10 +12,29 @@ class HomeDonorViewController: UIViewController {
     @IBOutlet weak var crearDonacion: UIButton!
     
     @IBOutlet weak var corregirDonacion: UIButton!
+    @IBOutlet weak var calendario: UIButton!
+    
+    let calendarService = CalendarService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Calendario2Segue" {
+            let controller = (segue.destination as! CalendarioViewController)
+            
+            calendarService.retrieveCalendar() {
+                (donation) in
+                DispatchQueue.main.async {
+                    controller.donacion = donation
+                    
+                    controller.tableView.reloadData()
+                }
+            }
+            
+        }
     }
 
 
