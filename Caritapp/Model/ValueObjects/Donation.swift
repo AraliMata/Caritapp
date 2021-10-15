a0ec7bb1f0c51ddddd55d5f1e5ca036e2eb44bfc
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2021-10-14 19:49:46 -0700

    BDA-17 Changing Donation definition to match backend

--- Caritapp/Model/ValueObjects/Donation.swift
+++ Caritapp/Model/ValueObjects/Donation.swift
@@ -8,20 +8,22 @@
 import Foundation
 
 public class Donation: Codable{
-    public let id: Int
+    //public let id: Int
     public let donador: String
     public let tienda: String
-    let kilos_donados: Float16
-    let kilos_recibidos: Float16
+    //let kilos_donados: Float16
+    let kilosDonados: Float16
+    //let kilos_recibidos: Float16
+    let kilosRecibidos: Float16
     let fecha: String
     
-    public init(id: Int, donador: String, tienda: String, kilos_donados: Float16, kilos_recibidos: Float16,  fecha: String){
-        self.id = id
+    public init(donador: String, tienda: String, kilos_donados: Float16, kilos_recibidos: Float16,  fecha: String){
+        //self.id = id
         self.donador = donador
         self.tienda = tienda
         self.fecha = fecha
-        self.kilos_recibidos = kilos_recibidos
-        self.kilos_donados = kilos_donados
+        self.kilosRecibidos = kilos_recibidos
+        self.kilosDonados = kilos_donados
         
     }
     
