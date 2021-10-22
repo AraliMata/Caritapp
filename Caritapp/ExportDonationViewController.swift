72541eec1a22bcb316d09a638a6a3d6773963186
Paulina Aldaco <paulina.aldaco.ro@gmail.com>
2021-10-21 20:51:11 -0700

    BDA-17 Adding integration and UI tests for export functionality

--- Caritapp/ExportDonationViewController.swift
+++ Caritapp/ExportDonationViewController.swift
@@ -11,13 +11,18 @@ class ExportDonationViewController: UIViewController {
     let productsService = LineaService()
     var exportedProducts: [Linea] = []
 
+    // UI labels
+    @IBOutlet weak var donadorLabel: UILabel!
+    @IBOutlet weak var tiendaLabel: UILabel!
+    @IBOutlet weak var fechaLabel: UILabel!
+    @IBOutlet weak var kilosDonadosLabel: UILabel!
+    @IBOutlet weak var kilosRecibidosLabel: UILabel!
+    @IBOutlet weak var destinoLabel: UILabel!
+    @IBOutlet weak var statusSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func exportDonation(_ sender: UIButton) {
-        print("button pressed")
         let semaphore = DispatchSemaphore(value: 0)
         productsService.retrieveProducts() {
             (products) in
@@ -29,6 +34,28 @@ class ExportDonationViewController: UIViewController {
             semaphore.signal()
         }
         semaphore.wait()
+        
+        // Update labels
+        self.donadorLabel.text = self.exportedProducts[0].donation?.donador
+        self.tiendaLabel.text = self.exportedProducts[0].donation?.tienda
+        self.fechaLabel.text = self.exportedProducts[0].donation?.fecha
+        self.kilosDonadosLabel.text = String(self.exportedProducts[0].donation!.kilosDonados)
+        self.kilosRecibidosLabel.text = String(self.exportedProducts[0].donation!.kilosRecibidos)
+        self.destinoLabel.text = self.exportedProducts[0].destino
+        
+        if self.exportedProducts[0].status == "Recibido" {
+            self.statusSwitch.isOn = true
+        }
+        else {
+            self.statusSwitch.isOn = false
+        }
+    }
+
+    @IBAction func exportDonation(_ sender: UIButton) {
+        print("button pressed")
+        
+        
+        // Decode JSON and create file
         let fileTool = FileTool()
         do {
             try fileTool.saveProductsFile(products: self.exportedProducts) {
