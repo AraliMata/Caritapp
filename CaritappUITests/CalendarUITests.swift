45cf674e8baace153f7b47cd562ae0a2b15b4581
Laurie Diego <laurievalediego@gmail.com>
2021-10-23 16:38:50 -0500

    UI Test BDA6

---
+++ CaritappUITests/CalendarUITests.swift
@@ -0,0 +1,74 @@
+//
+//  CalendarUITests.swift
+//  CaritappUITests
+//
+//  Created by user194078 on 10/23/21.
+//
+
+import Foundation
+
+import XCTest
+
+class CalendarUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    // Test UI for export functionality
+    func testCalendar() {
+        // Given
+        
+        //Launch the app
+        let app = XCUIApplication()
+        app.launch()
+        
+        // Set up variables for labels
+        let donadorLabel = app.staticTexts["donadorLabel"]
+        let kilosLabel = app.staticTexts["kilosLabel"]
+        let recepcionLabel = app.staticTexts["recepcionLabel"]
+
+        
+        // When
+
+        // Go to calendar view
+        let gotoHomeButton = app.buttons["Home Banco"]
+        gotoHomeButton.tap()
+        let gotoCalendarButton = app.buttons["Historial de donaciones"]
+        gotoCalendarButton.tap()
+        // Wait to change screen
+        let label = app.staticTexts["titleCal"]
+        let exists = NSPredicate(format: "exists == 1")
+        expectation(for: exists, evaluatedWith: label, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        // Wait to load values
+        let notEqualPredicate = NSPredicate(format: "label != %@", "---")
+        let serviceExpectation = XCTNSPredicateExpectation(predicate: notEqualPredicate, object: donadorLabel)
+        wait(for: [serviceExpectation], timeout: 10.0)
+        
+        // Assert correct label values
+        XCTAssertEqual(donadorLabel.label  , "Donador")
+        XCTAssertEqual(kilosLabel.label  , "Kilos")
+        XCTAssertEqual(recepcionLabel.label  , "Recepcion")
+        //Solo se prueban estos labels ya que al ser una lista no hay certeza de cuales valores estaran en la tabla
+    }
+
+}
